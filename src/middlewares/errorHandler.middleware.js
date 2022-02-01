2e5b89df1643b1e171f0ae11931cb4c331677508
Dejan Stojadinovic <dejan.dvte@gmail.com>
2022-02-01 16:36:58 +0100

    Minor update

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-vars */
-import pkg from 'joi';
 import { base } from '../config/index.js';
 import * as errors from '../utils/errors.util.js';
 
-const { ValidationError } = pkg;
 const { env } = base;
 
 const errorHandler = (err, req, res, next) => {
@@ -12,13 +10,6 @@ const errorHandler = (err, req, res, next) => {
 
   if (env !== 'production') {
     stackTrace = error.stack;
-  }
-
-  if (error instanceof ValidationError) {
-    error = new errors.HttpValidationError(
-      error.details[0].message,
-      error.details[0],
-    );
   }
 
   if (error?.data?.statusCode === 400) {
